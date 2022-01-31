354bc58ac75b8c4a51480dac10afd6d65d885b1b
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2022-01-31 22:24:11 +0600

    Add student portal page

--- includes/pages/header.php
+++ includes/pages/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>The Varendra Roots</title>
+    <title>The Varendra Spark</title>
     <!-- Link CSS -->
     <link rel="stylesheet" href="assets/css/bootstrap.css">
     <link rel="stylesheet" href="assets/css/styles.css">
@@ -19,7 +19,7 @@
     <section id="header">
         <nav class="navbar navbar-expand-lg navbar-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">The Varendra Roots</a>
+                <a class="navbar-brand" href="/">The Varendra Spark</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
                     aria-label="Toggle navigation">
@@ -27,9 +27,9 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Home</a>
+                        </li> -->
                         <li class="nav-item">
                             <a class="nav-link" href="#">Journals</a>
                         </li>
@@ -40,7 +40,7 @@
                             <a class="nav-link" href="#">Radio and Podcast</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Student Portal</a>
+                            <a class="nav-link" href="/varendra-portal/student-portal.php">Student Portal</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
