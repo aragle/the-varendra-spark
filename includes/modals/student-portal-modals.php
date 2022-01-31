c0bca8459e31f282bf502d4e365e966d12dd2516
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2022-02-01 01:39:24 +0600

    Update modal colors

--- includes/modals/student-portal-modals.php
+++ includes/modals/student-portal-modals.php
@@ -1,39 +1,4 @@
 <!-- Choose Student Portal Modals -->
-<style>
-.modal-content {
-    background-color: #eee;
-}
-
-.modal-title{
-    opacity: 0.8;
-    color: #c44d2d;
-}
-
-.btn-close{
-    color: #c44d2d;
-}
-
-.card{
-    background-color: transparent;
-}
-
-.card:hover{
-    opacity: 0.8;
-}
-
-a{
-    text-decoration: none;
-}
-
-.card-title {
-    color: #000;
-}
-
-.card-text{
-    color: #888;
-}
-</style>
-
 <div class="modal fade" id="studentPanelModal" tabindex="-1" aria-labelledby="studentPanelModalLabel"
     aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
--- includes/pages/header.php
+++ includes/pages/header.php
@@ -28,9 +28,9 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul class="navbar-nav">
-                        <!-- <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Home</a>
-                        </li> -->
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="/varendra-portal">Home</a>
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Journals</a>
                         </li>
