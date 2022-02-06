cdf9ff92402e24dbe73fe18bad65068e778ebf49
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2022-02-06 14:05:13 +0600

    Update footer links

--- includes/components/global/footer.php
+++ includes/components/global/footer.php
@@ -1,4 +1,9 @@
 <!-- Footer -->
+<style>
+p {
+    margin-bottom: 0px !important;
+}
+</style>
 <section id="footer">
     <footer class="bg-dark text-center text-white">
         <div class="container p-4">
@@ -25,7 +30,7 @@
                 <div class="row">
                     <!--Grid column 1-->
                     <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5 class="text-uppercase">Links</h5>
+                        <!-- <h5 class="text-uppercase">Links</h5>
 
                         <ul class="list-unstyled mb-0">
                             <li>
@@ -40,7 +45,9 @@
                             <li>
                                 <a href="#!" class="text-white">Link 4</a>
                             </li>
-                        </ul>
+                        </ul> -->
+                        <p style="margin-top: 40px; font-size: 35px; font-family: unique;">The Varendra Spark</p>
+                        <p>A Varendra University Online Portal</p>
                     </div>
 
                     <!--Grid column 2-->
@@ -85,20 +92,14 @@
 
                     <!--Grid column 4-->
                     <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5 class="text-uppercase">Links</h5>
+                        <h5 class="text-uppercase">Download Our Apps</h5>
 
                         <ul class="list-unstyled mb-0">
                             <li>
-                                <a href="#!" class="text-white">Link 1</a>
+                                <a href="#" class="text-white"><img style="width: 200px" src="assets/images/google-play-badge.png" alt="Get our apps on Play Store"></a>
                             </li>
                             <li>
-                                <a href="#!" class="text-white">Link 2</a>
-                            </li>
-                            <li>
-                                <a href="#!" class="text-white">Link 3</a>
-                            </li>
-                            <li>
-                                <a href="#!" class="text-white">Link 4</a>
+                            <a href="#" class="text-white"><img style="width: 175px" src="assets/images/app-store-badge.png" alt="Get our apps on App Store"></a>
                             </li>
                         </ul>
                     </div>
