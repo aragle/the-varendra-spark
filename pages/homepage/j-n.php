621cda840ed0c46eb56a3e470fae5ee18131a851
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2022-02-22 00:57:18 +0600

    fix demo text

--- pages/homepage/j-n.php
+++ pages/homepage/j-n.php
@@ -130,9 +130,15 @@
                         <h5>Bitcoin: A Peer-to-Peer Electronic Cash System</h5>
 
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, iste aliquid. Sed
-                            id nihil magni, sint vero provident esse numquam perferendis ducimus dicta
-                            adipisci iusto nam temporibus modi animi laboriosam?
+                          A purely peer-to-peer version of electronic cash would allow online
+                          payments to be sent directly from one party to another without going through a
+                          financial institution. Digital signatures provide part of the solution, but the main
+                          benefits are lost if a trusted third party is still required to prevent double-spending.
+                          We propose a solution to the double-spending problem using a peer-to-peer network.
+                          The network timestamps transactions by hashing them into an ongoing chain of
+                          hash-based proof-of-work, forming a record that cannot be changed without redoing
+                          the proof-of-work. The longest chain not only serves as proof of the sequence of
+                          events witnessed, but proof that it came from the largest pool of CPU power.
                         </p>
                     </a>
 
