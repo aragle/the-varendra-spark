7e0eb318c61df8d9b0fd585dd7228484778e5735
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2022-02-25 00:00:17 +0600

    hide rss feed

--- pages/homepage/j-n.php
+++ pages/homepage/j-n.php
@@ -322,12 +322,12 @@
                 </div>
               </div>
 
-                <div class="col-lg-4 col-md-12 mb-4 mb-lg-0 sidebar-news content-border">
+                <!-- <div class="col-lg-4 col-md-12 mb-4 mb-lg-0 sidebar-news content-border">
                     <h1 class="menu-title"> News </h1>
                     <rssapp-feed id="VUa5K3aJf31KZCtz"></rssapp-feed>
                     <script src="https://widget.rss.app/v1/feed.js" type="text/javascript" async></script>
                     <a href="news">
-                        <p class="menu-foot">See More News</p>
+                        <p class="menu-foot">See More News</p> -->
                     </a>
                 </div>
             </div>
