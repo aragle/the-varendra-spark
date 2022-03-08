fd8c1bc123e9100c7c987f6aafe771fb310dcfb5
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2022-03-08 19:34:46 +0600

    add file uploader

--- pages/study-resources/upload-resources.php
+++ pages/study-resources/upload-resources.php
@@ -3,3 +3,53 @@
     <a class="sub-nav">Upload Your Files</a>
     <a href="study-resources.php" class="sub-nav">Study Resources</a>
 </div>
+
+<section id="login" class="sign">
+    <div class="container py-3 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6">
+                <div class="card bg-dark text-white">
+                    <div class="card-body p-5">
+
+                        <div class="mb-md-0 mt-md-0 pb-5 ">
+                            <form action="upload-resources" method="post" enctype="multipart/form-data">
+                                <br>
+                                <div class="form-outline form-white mb-3">
+                                    <input type="file" name="fileToUpload" id="fileToUpload" class="form-control form-control-lg" />
+                                    <label class="form-label" for="fileToUpload">Select File</label>
+                                </div>
+
+                                <div class="form-outline form-white mb-2">
+                                    <select type="select" id="department" name="department" class="form-control form-control-lg">
+                                        <option value="">BBA</option>
+                                        <option value="">CSE</option>
+                                    </select>
+                                    <label class="form-label" for="department">Select Department</label>
+                                </div>
+
+                                <div class="form-outline form-white mb-2">
+                                    <select type="select" id="department" name="department" class="form-control form-control-lg">
+                                        <option value="">1st Semester</option>
+                                        <option value="">2nd Semester</option>
+                                    </select>
+                                    <label class="form-label" for="semester">Select Semester</label>
+                                </div>
+
+                                <div class="form-outline form-white mb-2">
+                                    <select type="select" id="department" name="department" class="form-control form-control-lg">
+                                        <option value="">CSE 101</option>
+                                        <option value="">PHY 101</option>
+                                    </select>
+                                    <label class="form-label" for="semester">Select Course</label>
+                                </div>
+
+                                <button name="login" class="btn btn-outline-light btn-lg px-5 submit-button"
+                                    type="submit">Upload</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- study-resources.php
+++ study-resources.php
@@ -1,5 +1,6 @@
 <?php
     include 'pages/common/header.php';
+    include 'controllers/study-resources/study-resources-upload-handler.php';
     include 'pages/study-resources/study-resources-index.php';
     include 'pages/common/footer.php';
 ?>
